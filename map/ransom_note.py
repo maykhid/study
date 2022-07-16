5bc602c6535446b1ad05e779e704937a36fba68c
Henry Ifebunandu <henryifebunandu@gmail.com>
2022-07-16 22:43:29 +0100

    added comments

--- map/ransom_note.py
+++ map/ransom_note.py
@@ -1,16 +1,18 @@
 def canConstruct(ransomNote: str, magazine: str) -> bool:
         mag = {}
         
+        # first for loop adds key (letter) and value (number of occurence) to the mag dict and increments for every occurence
         for i in magazine:
             if i in mag:
                 mag[i] += 1
             else:
                 mag[i] = 1
 
+        # second for loop reduces the value by 1 if letter is founc
         for j in ransomNote:
             if j in mag:
                 mag[j] -= 1
-                if(mag[j] == 0):
+                if(mag[j] == 0): # deletes key when value is zero
                     del mag[j]
             else:
                 return False    
